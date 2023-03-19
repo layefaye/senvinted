580205335982159b060c37a38099e386406b815d
layefaye <layefayenevrose@gmail.com>
2023-03-19 22:15:15 +0000

    first commit

---
+++ src/main/java/com/wagaanedev/senvinted/SenvintedApplication.java
@@ -0,0 +1,13 @@
+package com.wagaanedev.senvinted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenvintedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenvintedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wagaanedev/senvinted/SenvintedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagaanedev.senvinted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenvintedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
